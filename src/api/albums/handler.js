cb22c190fc37ed3d3feddde97d60eb86fbceb2a9
devmotive <ahmad.codespace@gmail.com>
2025-06-05 14:34:13 +0700

    feat: integrate Joi validation into album handlers and update routes

--- src/api/albums/handler.js
+++ src/api/albums/handler.js
@@ -1,104 +1,60 @@
 class AlbumsHandler {
-  constructor(service) {
+  constructor(service, validator) {
     this._service = service;
+    this._validator = validator;
 
     this.postAlbumHandler = this.postAlbumHandler.bind(this);
-    this.getAlbumsHandler = this.getAlbumsHandler.bind(this);
     this.getAlbumByIdHandler = this.getAlbumByIdHandler.bind(this);
     this.putAlbumByIdHandler = this.putAlbumByIdHandler.bind(this);
     this.deleteAlbumByIdHandler = this.deleteAlbumByIdHandler.bind(this);
   }
 
   postAlbumHandler(request, h) {
-    try {
-      const { name = 'untitled', year } = request.payload;
-      const albumId = this._service.addAlbum({ name, year });
+    this._validator.validateAlbumPayload(request.payload);
+    const { name = 'untitled', year } = request.payload;
+    const albumId = this._service.addAlbum({ name, year });
 
-      const response = h.response({
-        status: 'success',
-        message: 'Album added successfully',
-        data: {
-          albumId,
-        },
-      });
-      response.code(201);
-      return response;
-    } catch (error) {
-      const response = h.response({
-        status: 'fail',
-        message: error.message,
-      });
-      response.code(400);
-      return response;
-    }
+    const response = h.response({
+      status: 'success',
+      message: 'Album added successfully',
+      data: {
+        albumId,
+      },
+    });
+    response.code(201);
+    return response;
   }
 
-  getAlbumsHandler() {
-    const albums = this._service.getAllAlbums();
+  getAlbumByIdHandler(request) {
+    const { id } = request.params;
+    const album = this._service.getAlbumById(id);
     return {
       status: 'success',
       data: {
-        albums,
+        album,
       },
     };
   }
 
-  getAlbumByIdHandler(request, h) {
-    try {
-      const { id } = request.params;
-      const album = this._service.getAlbumById(id);
-      return {
-        status: 'success',
-        data: {
-          album,
-        },
-      };
-    } catch (error) {
-      const response = h.response({
-        status: 'fail',
-        message: error.message,
-      });
-      response.code(404);
-      return response;
-    }
+  putAlbumByIdHandler(request) {
+    this._validator.validateAlbumPayload(request.payload);
+    const { id } = request.params;
+    this._service.editAlbumById(id, request.payload);
+
+    return {
+      status: 'success',
+      message: 'Album updated successfully',
+    };
   }
 
-  putAlbumByIdHandler(request, h) {
-    try {
-      const { id } = request.params;
-      this._service.editAlbumById(id, request.payload);
+  deleteAlbumByIdHandler(request) {
+    const { id } = request.params;
+    this._service.deleteAlbumById(id);
 
-      return {
-        status: 'success',
-        message: 'Album updated successfully',
-      };
-    } catch (error) {
-      const response = h.response({
-        status: 'fail',
-        message: error.message,
-      });
-      response.code(404);
-      return response;
-    }
-  }
-
-  deleteAlbumByIdHandler(request, h) {
-    try {
-      const { id } = request.params;
-      this._service.deleteAlbumById(id);
-
-      return {
-        status: 'success',
-        message: 'Album deleted successfully',
-      };
-    } catch (error) {
-      const response = h.response({
-        status: 'fail',
-        message: error.message,
-      });
-      response.code(404);
-      return response;
-    }
+    return {
+      status: 'success',
+      message: 'Album deleted successfully',
+    };
   }
 }
 
