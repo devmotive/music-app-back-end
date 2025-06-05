2d205aefe05c5bb6c1e72b4456432c35cd45be52
devmotive <ahmad.codespace@gmail.com>
2025-06-05 20:23:28 +0700

    feat: add migration scripts for albums and songs tables

---
+++ migrations/1749115824848_create-table-albums.js
@@ -0,0 +1,48 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable('albums', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+      notNull: true,
+    },
+    name: {
+      type: 'VARCHAR(255)',
+      notNull: true,
+    },
+    year: {
+      type: 'INTEGER',
+      notNull: true,
+    },
+    created_at: {
+      type: 'TIMESTAMP',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+    updated_at: {
+      type: 'TIMESTAMP',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable('albums', {
+    ifExists: true,
+    cascade: true,
+  });
+};
---
+++ migrations/1749116162420_create-table-songs.js
@@ -0,0 +1,67 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable('songs', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+      notNull: true,
+    },
+    title: {
+      type: 'VARCHAR(255)',
+      notNull: true,
+    },
+    year: {
+      type: 'INTEGER',
+      notNull: true,
+    },
+    genre: {
+      type: 'VARCHAR(255)',
+      notNull: false,
+    },
+    performer: {
+      type: 'VARCHAR(255)',
+      notNull: true,
+    },
+    duration: {
+      type: 'INTEGER',
+      notNull: false,
+    },
+    albumId: {
+      type: 'VARCHAR(50)',
+      notNull: false,
+      references: 'albums',
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+    created_at: {
+      type: 'TIMESTAMP',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+    updated_at: {
+      type: 'TIMESTAMP',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable('songs', {
+    ifExists: true,
+    cascade: true,
+  });
+};
