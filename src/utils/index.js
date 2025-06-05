313f4febd794db2a86cca6c02d5a762875502791
devmotive <ahmad.codespace@gmail.com>
2025-06-05 20:24:08 +0700

    feat: add mapping functions for album and song models

---
+++ src/utils/index.js
@@ -0,0 +1,33 @@
+const mapDBToAlbumModel = ({
+  id, name, year, created_at, updated_at,
+}) => ({
+  id,
+  name,
+  year,
+  createdAt: created_at,
+  updatedAt: updated_at,
+});
+
+const mapDBToSongModel = ({
+  id,
+  title,
+  year,
+  genre,
+  performer,
+  duration,
+  album_id,
+  created_at,
+  updated_at,
+}) => ({
+  id,
+  title,
+  year,
+  genre,
+  performer,
+  duration,
+  albumId: album_id,
+  createdAt: created_at,
+  updatedAt: updated_at,
+});
+
+module.exports = { mapDBToAlbumModel, mapDBToSongModel };
