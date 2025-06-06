a0685928a393df1959f4710076cb51ae0ac27967
devmotive <ahmad.codespace@gmail.com>
2025-06-06 10:28:14 +0700

    feat: create migration for users table with id, username, password, and fullname fields

---
+++ migrations/1749179525605_create-table-users.js
@@ -0,0 +1,43 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable('users', {
+    id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      primaryKey: true,
+    },
+    username: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      unique: true,
+    },
+    password: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    fullname: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable('users', {
+    ifExists: true,
+    cascade: true,
+  });
+};
